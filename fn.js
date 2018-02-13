0ff6ab2d9671283db1cd46b0aae30b11c2a16f80
Pawel Ciupka <pawelciupka@gmail.com>
2018-02-13 21:27:50 +0100

    Dodano pierwszą wersje

--- fn.js
+++ fn.js
@@ -186,9 +186,54 @@ function showNextElement(elementName, circleName, numberOfElements, diff, direct
 }
 // END -> PAGE: PORTFOLIO
 
+// START -> PAGE: SKONTAKTUJ
+function validateForm() {
+  var f = document.forms["send_mail"];
+  var nameF = f.elements["name"].value;
+  var numberF = f.elements["number"].value;
+  var emailF = f.elements["mail"].value;
+  var topicF = f.elements["topic"].value;
+  var messageF = f.elements["message"].value;
+
+  var ok = true;
+  var errorMessage = "";
+
+  if(nameF == null || nameF == "" || nameF.length < 6) {
+    errorMessage += "Nieprawidłowe imię i nazwisko. \n"
+    ok = false;
+  }
+
+  if(numberF == null || numberF == "" || numberF.length != 9) {
+    errorMessage += "Nieprawidłowy numer. \n"
+    ok = false;
+  } 
+  
+  if(emailF == null || emailF == "") {
+    errorMessage += "Nieprawidłowy e-mail. \n"
+    ok = false;
+  }
+
+  if(topicF == null || topicF == "" || topicF.length < 3) {
+    errorMessage += "Nieprawidłowy temat. \n"
+    ok = false;
+  }
+
+  if(messageF == null || messageF == "" || messageF.length < 10) {
+    errorMessage += "Nieprawidłowa wiadomość. \n"
+    ok = false;
+  }
+
+  if(ok) {
+    alert("Wysłałeś wiadomość! \nPozwól, że przeniosę cię na stronę główną.");
+    return true;
+  } else {
+    alert(errorMessage);
+    return false;
+  }
+}
 
 
-
+// END -> PAGE: SKONTAKTUJ
 
 
 /*
---
+++ php/sendMail.php
@@ -0,0 +1,58 @@
+<?php
+
+$name = $_POST["name"];
+$number = $_POST["number"];
+$mail = $_POST["mail"];
+$topic = $_POST["topic"];
+$msg = $_POST["message"];
+
+$to = "pawelciupka@gmail.com";
+$them = "Czyżby zlecenie?";
+
+$headers =  'MIME-Version: 1.0' . "\r\n"; 
+$headers .= 'From: '.$name.' <'.$mail.'>' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+
+$message = '
+<html>
+<head lang="pl">
+    <meta charset="utf-8">
+    <style>
+        span {
+            color: #000;
+        }
+        .info {
+            color: #3180fd;
+            font-weight: bold;
+            margin: 0 10px 0 0;
+        }
+        p {
+            margin: 0;
+            text-align: justify;
+        }
+    </style>
+</head>
+<body>
+    <span class="info">Imię i nazwisko: </span>
+    <span>'.$name.'</span>
+    <br>
+    <span class="info">Numer telefonu </span>
+    <span>'.$number.'</span>
+    <br>
+    <span class="info">E-mail: </span>
+    <span>'.$mail.'</span>
+    <br>
+    <span class="info">Temat: </span>
+    <span>'.$topic.'</span>
+    <br>
+    <span class="info">Wiadomość: </span>
+    <p>'.$msg.'</p>
+</body>
+</html>';
+
+echo $message;
+//mail($to, $them, $message, $headers);
+
+//header('location: ../index.html');
+
+?>
